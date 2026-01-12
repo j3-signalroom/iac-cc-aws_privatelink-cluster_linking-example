be102b9a5e9233394f2a583c8c3b915343d2125c
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2026-01-12 13:18:36 +0100

    Resolved #84.

--- terraform/setup-confluent-cluster_linking.tf
+++ terraform/setup-confluent-cluster_linking.tf
@@ -164,6 +164,9 @@ resource "confluent_kafka_mirror_topic" "stock_trades_mirror" {
     confluent_cluster_link.sandbox_to_shared,
     module.cluster_linking_sandbox_cluster_app_manager_api_key.active_api_key,
     module.cluster_linking_shared_cluster_app_manager_api_key.active_api_key,
-    confluent_kafka_topic.source_stock_trades
+    confluent_kafka_topic.source_stock_trades,
+    confluent_private_link_attachment_connection.sandbox_cluster_plattc,
+    confluent_private_link_attachment_connection.shared_cluster_plattc,
+    confluent_private_link_attachment_connection.tfc_agent_plattc
   ]
 }
