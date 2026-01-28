dcaa3d4a25b8f4f5f04d4c8693f1821fb0baaaa8
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2026-01-27 20:12:50 -0500

    See #141.  Added the new resources.

---
+++ terraform/modules/aws-vpc-confluent-privatelink/setup-aws-vpc-security_group_rules.tf
@@ -0,0 +1,58 @@
+# ============================================================================
+# SECURITY GROUP RULES FOR THE VPC
+# ============================================================================
+resource "aws_security_group" "privatelink" {
+  name        = "ccloud-privatelink_${local.network_id}_${aws_vpc.privatelink.id}"
+  description = "Confluent Cloud Private Link Security Group for ${var.dns_domain}"
+  vpc_id      = aws_vpc.privatelink.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  
+  tags = {
+    Name        = "ccloud-privatelink-${local.network_id}"
+    VPC         = aws_vpc.privatelink.id
+    Environment = data.confluent_environment.privatelink.display_name
+  }
+}
+
+resource "aws_security_group_rule" "allow_https" {
+  description       = "HTTPS from VPC, TFC Agent VPCs, and VPN clients"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [var.tfc_agent_vpc_cidr, var.vpn_client_vpc_cidr, var.vpc_cidr]
+  security_group_id = aws_security_group.privatelink.id
+}
+
+resource "aws_security_group_rule" "allow_kafka" {
+  description       = "Kafka from VPC, TFC Agent VPCs, and VPN clients"
+  type              = "ingress"
+  from_port         = 9092
+  to_port           = 9092
+  protocol          = "tcp"
+  cidr_blocks       = [var.tfc_agent_vpc_cidr, var.vpn_client_vpc_cidr, var.vpc_cidr]
+  security_group_id = aws_security_group.privatelink.id
+}
+
+resource "aws_security_group_rule" "allow_dns_udp" {
+  description       = "DNS (UDP) from VPC, TFC Agent VPCs, and VPN clients"
+  type              = "ingress"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "udp"
+  cidr_blocks       = [var.tfc_agent_vpc_cidr, var.vpn_client_vpc_cidr, var.vpc_cidr]
+  security_group_id = aws_security_group.privatelink.id
+}
+
+resource "aws_security_group_rule" "allow_dns_tcp" {
+  description       = "DNS (TCP) from VPC, TFC Agent VPCs, and VPN clients"
+  type              = "ingress"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "tcp"
+  cidr_blocks       = [var.tfc_agent_vpc_cidr, var.vpn_client_vpc_cidr, var.vpc_cidr]
+  security_group_id = aws_security_group.privatelink.id
+}
