462a082e27e2c688868cd7fef2d5be2b7dd3231d
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2026-02-08 08:51:09 -0500

    See #205.  Changed the name of a variable to be more readable.

--- deploy.sh
+++ deploy.sh
@@ -30,23 +30,23 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NO_COLOR='\033[0m'
 
 # Function to print colored output
 print_info() {
-    echo -e "${GREEN}[INFO]${NC} $1"
+    echo -e "${GREEN}[INFO]${NO_COLOR} $1"
 }
 
 print_warn() {
-    echo -e "${YELLOW}[WARN]${NC} $1"
+    echo -e "${YELLOW}[WARN]${NO_COLOR} $1"
 }
 
 print_error() {
-    echo -e "${RED}[ERROR]${NC} $1"
+    echo -e "${RED}[ERROR]${NO_COLOR} $1"
 }
 
 print_step() {
-    echo -e "${BLUE}[STEP]${NC} $1"
+    echo -e "${BLUE}[STEP]${NO_COLOR} $1"
 }
 
 # Configuration folders
